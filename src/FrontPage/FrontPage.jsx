ee6490d412ca6185afc8239e6e36c1bcdc88231a
Jostein24sfj <jostein24sfj@gmail.com>
2025-03-21 09:25:17 +0100

    removed code that was not in use. Added   min-height: 100vh; to aboutMe

--- src/FrontPage/FrontPage.jsx
+++ src/FrontPage/FrontPage.jsx
@@ -1,20 +1,18 @@
-import { useState } from "react";
-import "./FrontPage.css"
+import "./FrontPage.css";
 
 export default function FrontPage() {
-    return (
-      <>
-        <section className="frontPageContent">
-          <img className="profilePic" src="/images/profilePic.png" alt="" />
-          <div>
-            <h1>Jostein Westrum</h1>
-            <p className="frontPageText">
-              Mitt navn er Jostein Westrum. Dette er forsøk nr 2 på å lage en
-              portefolie. Denne gangen lages det i React.
-            </p>
-          </div>
-        </section>
-      </>
-    );
-  }
-  
+  return (
+    <>
+      <section className="frontPageContent">
+        <img className="profilePic" src="/images/profilePic.png" alt="" />
+        <div>
+          <h1>Jostein Westrum</h1>
+          <p className="frontPageText">
+            Mitt navn er Jostein Westrum. Dette er forsøk nr 2 på å lage en
+            portefolie. Denne gangen lages det i React.
+          </p>
+        </div>
+      </section>
+    </>
+  );
+}
